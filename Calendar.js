a3b7c2bbd6c363939bcba99d64ad0982ab0222a4
Matt <hpotter134@gmail.com>
2011-11-24 18:56:05 -0500

    More Calendar brainstorming. Added dates and striped colors. Clicking a cell now alerts the Day and date. Awesomesauce.

--- Calendar.js
+++ Calendar.js
@@ -1,21 +1,57 @@
 /* Calendar.js */
 $(document).ready(function(){
+	/* GLOBALS: */
 	lastCalendarStyle = ""; // this String will record the color of the td that was moused-over
+	startCell = 3; // Which day of the week is the first day of the month? 0 is Sunday.
+	
 	createEmptyCalendar();
-	$("td").hover(function(){ // mouseenter
-		lastCalendarStyle = ($(this).attr("style")) ? $(this).attr("style") : "" ;
-		$(this).attr("style","background-color:#f5f5f5;");
-	} , function(){ // mouseleave
-		$(this).attr("style", lastCalendarStyle);
-	});
+	
+	/* EVENT HANDLERS: */
+	$("td").hover(
+		/* mouseenter */
+		function(){
+			lastCalendarStyle = ($(this).attr("style")) ? $(this).attr("style") : "" ;
+			$(this).attr("style","background-color:#f5f5f5;");
+		} ,
+		/* mouseleave */
+		function(){
+			$(this).attr("style", lastCalendarStyle);
+		}
+	); // end td hover
+	
+	$("td").click(function(){
+		if ($(this).attr("id") != "calendarcell"){
+			var date = $(this).attr("id").substring(12); // ex. id is calendarcell7, date is 7
+		
+			/* find which day of the week corresponds using the table headers */
+			var day = $($("th")[(date-1+startCell)%7]).text(); 
+			
+			alert("This is " +day +", the " +date +"th.");
+		}
+	}); // end td click
 });
 
 function createEmptyCalendar(){
-	for (var i = 0; i < 4; i++){
-		$("body tbody").append("<tr></tr>");
-		for (var k = 0; k < 7; k++){
-			var sty = ((i+k)%2 == 1) ? " style=\"background-color:#f9f9f9\"" : "";
-			$("body tbody tr").last().append("<td"+sty +"></td>");
+	//startCell = 1; //see GLOBALS at top of file
+	var daysNum = 31; // number of days in the month
+	var rowsNum = (daysNum+startCell)/7; // number of rows needed
+	for (var i = 0; i < rowsNum; i++){ // week loop (4, 5 or 6)
+		$("body tbody").append("<tr></tr>"); // New Row
+		
+		for (var k = 0; k < 7; k++){ // day loop (7)
+		
+			var x = (i*7)+k+1-startCell; // Calculate current date from position in calendar
+			
+			/* if the date is invalid, the cell will be blank and the id will be "calendarcell" */
+			if (x > 31 || x < 1){
+				x = "";
+				id = "";
+			} else {
+				var id = " id=\"calendarcell"+x+"\""; // if the date is valid, id is calendarcell"date"
+			}
+			
+			var sty = ((i+k)%2 == 1) ? " style=\"background-color:#f9f9f9\"" : ""; // "zebra-striped idea"
+			$("body tbody tr").last().append("<td" +id +sty +">"+x+"</td>");
 		}
 	}
 }
