e6a1194c7f2784b207f44fc989981e4b0fafa6a1
Matt <hpotter134@gmail.com>
2011-12-19 19:59:50 -0500

    Got rid of John's dashes...they were annoying... threw all the jscolor jazz in the dump Calendar.html had some colors changed thanks to our amazing Graphics Designer

--- Week.js
+++ Week.js
@@ -18,7 +18,7 @@ $(document).ready(function(){
 	createEmptyWeek("gray");
 	
 	var randNum = Math.floor(Math.random()*3);
-	$($(".Eblock")[randNum]).append('<div class="alert-message info" style="height:30"><h4>QUIZ</h4></div>');
+	$($(".E\-block")[randNum]).append('<div class="alert-message info" style="height:30"><h4>QUIZ</h4></div>');
 
 	
 	/* EVENT HANDLERS: */
@@ -41,16 +41,7 @@ $(document).ready(function(){
 		}
 	); // end td hover
 	$("#CalendarTable td").click(function(){
-		var tdClass = $(this).attr("class");
-		$("#colorInput").click(); // triggers JSColor
-		console.log(jscolor.picker);
-		$(jscolor.picker.box).mousedown(function(e){
-			$("#colorInput").change(function(){
-				var tdColor = $("#colorInput").css('background-color').toLowerCase();
-				changeColor(tdClass, tdColor);
-			});
-		});
-		// jscolor.picker.owner.hidePicker();
+		//$(this).modal('toggle');
 	}); // end td click
 	
 });
@@ -66,7 +57,7 @@ function createEmptyWeek(weekColor){
 	for( var i = 0; i < 6; i++ ){
 		$("#CalendarTable tbody").append("<tr></tr>"); // New Row
 		for (var k = 0; k <= 6; k++){
-			var tdClass = weekStructure[k][i] + "block";
+			var tdClass = weekStructure[k][i] + "-block";
 			$("#CalendarTable tbody tr").last().append("<td class=\""+tdClass +"\">"+tdClass+"</td>");
 		}
 	}
@@ -74,21 +65,21 @@ function createEmptyWeek(weekColor){
 
 function getWeekStructure(weekColor){
 	var maroonWeek = new Array();
-		maroonWeek[0] = new Array('A-','C-','D-','E-','F-','G-');
-		maroonWeek[1] = new Array('B-','C-','D-','F-','G-','H-');
-		maroonWeek[2] = new Array('A-','B-','E-','F-','activity-','');
-		maroonWeek[3] = new Array('A-','C-','D-','E-','G-','H-');
-		maroonWeek[4] = new Array('B-','A-','C-','F-','G-','H-');
-		maroonWeek[5] = new Array('No School-','','','','','');
-		maroonWeek[6] = new Array('No School-','','','','','');
+		maroonWeek[0] = new Array('A','C','D','E','F','G');
+		maroonWeek[1] = new Array('B','C','D','F','G','H');
+		maroonWeek[2] = new Array('A','B','E','F','activity','');
+		maroonWeek[3] = new Array('A','C','D','E','G','H');
+		maroonWeek[4] = new Array('B','A','C','F','G','H');
+		maroonWeek[5] = new Array('No School','','','','','');
+		maroonWeek[6] = new Array('No School','','','','','');
 	var grayWeek = new Array();
-		grayWeek[0] = new Array('A-','C-','D-','E-','F-','H-');
-		grayWeek[1] = new Array('B-','C-','D-','E-','G-','H-');
-		grayWeek[2] = new Array('A-','B-','E-','F-','activity','');
-		grayWeek[3] = new Array('B-','C-','D-','E-','G-','H-');
-		grayWeek[4] = new Array('B-','A-','D-','F-','G-','H-');
-		grayWeek[5] = new Array('No School-','','','','','');
-		grayWeek[6] = new Array('No School-','','','','','');
+		grayWeek[0] = new Array('A','C','D','E','F','H');
+		grayWeek[1] = new Array('B','C','D','E','G','H');
+		grayWeek[2] = new Array('A','B','E','F','activity','');
+		grayWeek[3] = new Array('B','C','D','E','G','H');
+		grayWeek[4] = new Array('B','A','D','F','G','H');
+		grayWeek[5] = new Array('No School','','','','','');
+		grayWeek[6] = new Array('No School','','','','','');
 		
 	return (weekColor == "maroon") ? maroonWeek.slice(0) : grayWeek.slice(0);
 }
