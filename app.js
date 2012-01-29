2b630b778800c922255d97cf34060f51fee010c2
Jason Teplitz <jteplitz602@gmail.com>
2012-01-29 00:47:56 -0500

    storing session in mongo, make sure to install connect-mongodb using npm

--- app.js
+++ app.js
@@ -3,63 +3,65 @@
  * Module dependencies.
  */
 
-var express  = require('express')
-  , routes   = require('./routes')
-  , mongoose = require("mongoose") 
-  , schemas  = require("./schemas");
+var express         = require('express')
+  , routes          = require('./routes')
+  , mongoose        = require("mongoose")
+  , mongoStore      = require("connect-mongodb")
+  , schemas         = require("./schemas");
+
+var app        = module.exports = express.createServer();
+var mongoURI   = process.env.MONGOLAB_URI || "mongodb://127.0.0.1/calendar";
+mongoose.connect(mongoURI);
+
+mongoose.connection.on("open", function(){
+  app.configure(function(){
+    app.set('views', __dirname + '/views');
+    app.set('view engine', 'jade');
+    app.set("view options", {"layout": false});
+    app.use(express.bodyParser());
+    app.use(express.methodOverride());
+    app.use(express.cookieParser());
+    app.use(express.session( {store: new mongoStore({db: mongoose.connection.db}), cookie: {httpOnly: true, maxAge: 604800000}, secret: process.env.secret || "The computer science club will rise again... and reclaim B204" }));
+    app.use(app.router);
+    app.use(express.static(__dirname + '/public'));
+  });
+  app.configure('development', function(){
+    app.use(express.errorHandler({ dumpExceptions: true, showStack: true })); 
+  });
+
+  app.configure('production', function(){
+    app.use(express.errorHandler()); 
+  });
+
+  // Routes
+
+  app.get('/', routes.index);
+  app.get("/monthly", routes.monthly);
+  app.get("/weekly", routes.weekly);
+
+  app.get("/logout", routes.logout);
+  app.get("/login", routes.loginPage);
+  app.get("/signup", routes.createAccount);
+
+  app.post("/createAccount", routes.createUser);
+  app.post("/login", routes.login);
+
+  app.post('/setup_blocks', routes.setupBlocks);
+  app.get("/setup", routes.setup);
+
+  // events
+  app.post("/event", routes.createEvent);
+  app.post("/event/:eventId", routes.modifyEvent);
+  app.delete("/event/:eventId", routes.deleteEvent);
+
+  app.post("/createClass", routes.createClass);
+  app.post("/addStudent", routes.addStudent);
 
 
-var app = module.exports = express.createServer();
-console.log(process.env.MONGOLAB_URI || "mongodb://127.0.0.1/calender");
-mongoose.connect(process.env.MONGOLAB_URI || "mongodb://127.0.0.1/calendar");
+  app.listen(process.env.PORT || 3000);
+  console.log("Express server listening on port %d in %s mode", app.address().port, app.settings.env);
 
+
+
+});
 // Configuration
-
-app.configure(function(){
-  app.set('views', __dirname + '/views');
-  app.set('view engine', 'jade');
-  app.set("view options", {"layout": false});
-  app.use(express.bodyParser());
-  app.use(express.methodOverride());
-  app.use(express.cookieParser());
-  app.use(express.session({ cookie: {path: "/", httpOnly: true, maxAge: 604800000}, secret: process.env.secret || "The computer science club will rise again... and reclaim B204" }));
-  app.use(app.router);
-  app.use(express.static(__dirname + '/public'));
-});
-
-app.configure('development', function(){
-  app.use(express.errorHandler({ dumpExceptions: true, showStack: true })); 
-});
-
-app.configure('production', function(){
-  app.use(express.errorHandler()); 
-});
-
-// Routes
-
-app.get('/', routes.index);
-app.get("/monthly", routes.monthly);
-app.get("/weekly", routes.weekly);
-
-app.get("/logout", routes.logout);
-app.get("/login", routes.loginPage);
-app.get("/signup", routes.createAccount);
-
-app.post("/createAccount", routes.createUser);
-app.post("/login", routes.login);
-
-app.post('/setup_blocks', routes.setupBlocks);
-app.get("/setup", routes.setup);
-
-// events
-app.post("/event", routes.createEvent);
-app.post("/event/:eventId", routes.modifyEvent);
-app.delete("/event/:eventId", routes.deleteEvent);
-
-app.post("/createClass", routes.createClass);
-app.post("/addStudent", routes.addStudent);
-
-
-app.listen(process.env.PORT || 3000);
-console.log("Express server listening on port %d in %s mode", app.address().port, app.settings.env);
-
