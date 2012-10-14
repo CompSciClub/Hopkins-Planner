e99e0f24e9e855a3f21c547adbf1aebb349f839e
Jason Teplitz <Jason@Jason-Teplitzs-Mac.local>
2012-10-14 15:40:04 -0400

    fixed small issue with the jade code for the week

--- routes/index.js
+++ routes/index.js
@@ -6,17 +6,19 @@
     Weekly: require("./Weekly"),
     Login: require("./Login"),
     Signup: require("./Signup"),
-    Logout: require("./Logout")
+    Logout: require("./Logout"),
+    Event: require("./Event")
   };
 
   // route, function, mongo, auth level, methods
   // auth level: 0 = not required, 1 = required
   var routeList = [
-    ["/signup",          Routes.Signup,       1, 0, ["get", "post"]],
-    ["/",                Routes.Root,         0, 0, ["get"        ]],
-    ["/weekly/:offset?", Routes.Weekly,       1, 1, ["get"        ]],
-    ["/login",           Routes.Login,        1, 0, ["get", "post"]],
-    ["/logout",          Routes.Logout,       0, 1, ["get"        ]]
+    ["/signup",          Routes.Signup,       1, 0, ["get", "post"          ]],
+    ["/",                Routes.Root,         0, 0, ["get"                  ]],
+    ["/weekly/:offset?", Routes.Weekly,       1, 1, ["get"                  ]],
+    ["/login",           Routes.Login,        1, 0, ["get", "post"          ]],
+    ["/logout",          Routes.Logout,       0, 1, ["get"                  ]],
+    ["/event/:eventId?", Routes.Event,        1, 1, [       "post", "delete"]]
   ];
 
   exports.list = routeList;
