11508be9e5ecc32056a5e6e2369169a4d7ad5a70
Matt <hpotter134@gmail.com>
2012-12-05 12:55:31 -0500

    Revert Bug Fix (It didn't Work)
    https://github.com/CompSciClub/Hopkins-Planner/commit/be066eff433ec2cfc0698dc9d2f8554ff1576080

--- app/Week.js
+++ app/Week.js
@@ -13,7 +13,7 @@
       ["D", "D", "E", "D", "C"],
       ["E", "F", "F", "E", "F"],
       ["F", "G", "Activities", "G", "G"],
-      ["G", "H", "BLANK", "H", "H"]
+      ["G", "H", "H", "H"]
     ];
     var grayWeek = [
       ['A', 'B', 'A', 'B', 'B', "Saturday", "Sunday"],
@@ -21,7 +21,7 @@
       ["D", "D", "E", "D", "D"],
       ["E", "E", "F", "E", "F"],
       ["F", "G", "Activities", "G", "G"],
-      ["H", "H", "BLANK", "H", "H"]
+      ["H", "H", "H", "H"]
     ];
     var week = (getWeek(date) === "maroon") ? maroonWeek.slice(0) : grayWeek;
     Holiday.find({timestamp: {$gte: date.getTime(), $lte: date.getTime() + 604800000}}, function(err, holidays){
