aeb34226559e95df4d8fd8ccb227a9d93355278a
Iwscskfw372 <nailen.matschke@gmail.com>
2013-01-29 11:59:07 -0500

    Fixes login.

--- routes/AuthCallback.js
+++ routes/AuthCallback.js
@@ -26,6 +26,7 @@
     
     controller.loginUser(mode, email, id, function(err, user){
       if (err){
+ 	console.log("error logging in user", err);
         switch (err.type){
           case "cancel":
             res.redirect("/");
