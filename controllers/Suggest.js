10b7a5174bcb37e8bc0dd9a0cbc4dc80183a49c5
Iwscskfw372 <nailen.matschke@gmail.com>
2013-01-30 14:02:59 -0500

    Fixed a couple of layout inconsistencies and added a not really working feedback page.

--- controllers/Suggest.js
+++ controllers/Suggest.js
@@ -3,7 +3,7 @@
   "use strict";
 
   var base      = require("./base.js"),
-      Crypto    = require("encrypto").Crypto,
+      Crypto    = require("ezcrypto").Crypto,
       _         = require("underscore"),
       ViewClass = require("../views/Suggest.js");
 
@@ -50,6 +50,26 @@
       cb();
     });
   };
+ 
+ _ptype.addSuggestion = function(grade, name, Title, feedback, cb){
+   var self = this;
+   User.find({_id: self.uid}, function(err, users){
+     var user = users[0];
+     if (err || !user){
+       console.log("Error finding user");
+       return cb(true);
+   }
+     
+             
+     console.log(user.feedback);
+     user.Title = Title;
+     user.grade  = grade;
+     user.name   = name;
+     user.feedback = feedback;
+           
+     user.save(cb);
+   });
+ };
   
   module.exports = SuggestCtrl;
 }());
--- routes/Suggest.js
+++ routes/Suggest.js
@@ -3,7 +3,8 @@
   
   var _ = require("underscore");
   
-  var ControllerClass = require("../controllers/Suggest.js");
+  var ControllerClass = require("../controllers/Suggest.js"),
+ email           = require("../app/email.js");
   
   var dispatch, handler,
   handleGet, handlePost;
@@ -20,6 +21,9 @@
 
   handlePost = function(req, res, next){
     var control = new ControllerClass(req.session.userId);
+    control.addSuggestion(req.body.grade, req.body.name, null, null, function(){
+    })
+ email.sendEmail("hopkins-csc@groups.google.com", "nate@NATE.ORG", "FEEDBACK", "path/to/email/template.jade", templateVars);
   };
 
   dispatch = {GET: handleGet, POST: handlePost};
