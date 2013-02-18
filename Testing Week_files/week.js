6caa1b70fbc12b7a4505265197bfcf019587caa3
Matt <hpotter134@gmail.com>
2013-02-18 18:31:09 -0500

    updateMobileScreen

--- Testing Week_files/week.js
+++ Testing Week_files/week.js
@@ -102,6 +102,7 @@ $(document).ready(function(){
   $("#saveButton").click(function(){
       updateBlock();
       createEvent(modalTypeVar);
+	  updateMobileScreen();
   });
   $("#deleteButton").click(function(){
     deleteEvent();
